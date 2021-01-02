ed74117a39d26f18ee1d13c1ca0fc7d1f5742040
Dmitriy <disa.dimon4ik.1999@gmail.com>
2021-01-02 23:38:40 +0200

    Посмотрел видео и разобрался с написанием условий  циклов, понял что необходимо включать не стандартное мышлление и подход. Осталось только выполнить задачу используя циклы do и do-while

--- script.js
+++ script.js
@@ -10,27 +10,16 @@ const personalMovieDB = {
   privat: false
 };
 
-// const a = prompt("Один из последних просмотренных фильмов?", ""),
-//       b = prompt("На сколько оцените его?", ""),
-//       c = prompt("Один из последних просмотренных фильмов?", ""),
-//       d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
 
-for (let i = 1; i < 3; i++) {
-  var a = prompt("Один из последних просмотренных фильмов?", ""),
-      c = a;
-  var b = prompt("На сколько оцените его?", ""),
-      d = b;
-  
-  if (a || b || c || d === "") {
-    do {
-      
-    }
-  }
-  else if (a, b, c, d == null) {
-    continue;
-  }
-  else if (a.length, b.length, c.length, d.length >= 50){
-    continue;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("Done");
+  } else {
+    i--;
+    console.log("Error!");
   }
 }
 
@@ -43,9 +32,5 @@ if (personalMovieDB.count < 10){
   } else {
     console.log("Произошла ошибка");
   }
-
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
