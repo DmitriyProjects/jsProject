e59ee2f90fc07206d465e289b43c41ebb56aa307
Dmitriy <ddcommunity19@gmail.com>
2021-01-02 12:18:34 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+const isCheked = true,
+      isClose = false;
+
+console.log(isCheked && isClose);
