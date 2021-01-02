600cdc90bec90a3a23173855bb4eed9dba60f24f
Dmitriy <disa.dimon4ik.1999@gmail.com>
2021-01-02 14:39:55 +0200

    Создание обьекта personalMovieDB и поместил в него следующие значения: count, movies, actors, genres и  privat

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 console.log(numberOfFilms);
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
