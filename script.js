53d2d50fabdefd184f06cf9a14f60d7877fa89f1
Denysenko Dmitriy <denysenkodmitriy53@gmail.com>
2021-01-06 00:13:25 +0200

    Очередное практическое занятие, начинаю мыслить и писать код правильно

--- script.js
+++ script.js
@@ -1,34 +1,111 @@
 "use strict";
 
-const options = {
-  name: 'test',
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: 'black',
-    bg: 'red'
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//   a: 5,
+//   b: 1
+// };
+
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+//   let objCopy = {};
+
+//   let key;
+//   for (key in mainObj) {
+//     objCopy[key] = mainObj[key];
+//   }
+
+//   return objCopy;
+// }
+
+// const numbers = {
+//   a: 2,
+//   b: 5,
+//   c: {
+//     x: 7,
+//     y: 4
+//   }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+
+// // console.log(newNumbers);
+// // console.log(numbers);
+
+// const add = {
+//   d: 17,
+//   e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// // console.log(add);
+// // console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'qwerty';
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ["a", "b"];
+
+// const newAarray = [...array];
+
+// const q = {
+//   one: 1,
+//   two: 2
+// };
+
+// const newObj = {...q};
+
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: function() {
+    console.log("Hello");
   }
 };
 
-console.log(Object.keys(options).length);
+const john = Object.create(soldier);
 
-// console.log(options.name);
+// john.__proto__ = soldier;
 
-// delete options.name;
+// Object.setPrototypeOf(john, soldier);
 
-// console.log(options);
 
-// let counter = 0;
-// for (let key in options) {
-//   if (typeof (options[key]) === 'object') {
-//     for (let i in options[key]) {
-//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//       counter++;
-//     }
-//   } else {
-//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//     counter++;
-//   }
-// }
-
-// console.log(counter);
+john.sayHello();
